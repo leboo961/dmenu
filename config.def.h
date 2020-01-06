1aba710f62fb35eb332d90fd34e2d1efcd28251a
Fadi EL HADDAD <fadi.haddad.dev@gmail.com>
2020-01-06 12:17:05 +0000

    center patch added and border still not working

--- config.def.h
+++ config.def.h
@@ -3,6 +3,7 @@
 
 static int instant = 0;
 static int topbar = 1;                      /* -b  option; if 0, dmenu appears at bottom     */
+static int centered = 0;                    /* -c option; centers dmenu on screen */
 /* -fn option overrides fonts[0]; default X11 font or font set */
 static const char *fonts[] = {
 	"monospace:size=10"
@@ -22,3 +23,6 @@ static unsigned int lines      = 0;
  * for example: " /?\"&[]"
  */
 static const char worddelimiters[] = " ";
+
+/* Size of the window border */
+static const unsigned int border_width = 5;
